687521ddd7390a1f263ccbc3a21834d65d0b402b
nicepolo <nicepolo1222@gmail.com>
2026-02-25 00:40:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
 const BOT_TOKEN     = process.env.BOT_TOKEN     || "8753253331:AAGOZe58YJnHwjJU4i2Gf5aAPwT2rPIuqEU";
-const ANTHROPIC_KEY = process.env.ANTHROPIC_KEY || "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";
+const ANTHROPIC_KEY = process.env.ANTHROPIC_KEY || "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";
 const WEBAPP_URL    = process.env.WEBAPP_URL    || "https://nicepolo.github.io/shenhuiai";
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
